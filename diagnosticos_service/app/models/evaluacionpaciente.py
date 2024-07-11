5f35e94ffa074c322a6b23be7bfc397f77926522
Amy <amy.huaman2001@gmail.com>
2024-07-10 22:04:24 -0500

    Actualizacion Diagnosticos services

--- diagnosticos_service/app/models/evaluacionpaciente.py
+++ diagnosticos_service/app/models/evaluacionpaciente.py
@@ -1,19 +1,25 @@
 from app.extensions import db
 from dataclasses import dataclass
+from datetime import datetime, timezone
 
 @dataclass
 class EvaluacionPaciente(db.Model):
     __tablename__ = 'evaluacionpaciente'
     id_evaluacion = db.Column(db.Integer, primary_key=True)
-    id_diagnostico = db.Column(db.Integer, db.ForeignKey('diagnostico.id_diagnostico', onupdate='RESTRICT', ondelete='RESTRICT'))
-    especialista_id = db.Column(db.Integer, nullable=False)
-    resultado_id = db.Column(db.Integer, nullable=False)
-    fecha_evaluacion = db.Column(db.DateTime, nullable=False)
+    id_diagnostico = db.Column(db.Integer, db.ForeignKey('diagnostico.id_diagnostico'), nullable=False)
+    especialista_id = db.Column(db.Integer, db.ForeignKey('especialistas.especialista_id'), nullable=False)
+    resultado_id = db.Column(db.Integer, db.ForeignKey('test_resultado.resultado_id'), nullable=False)
+    fecha_evaluacion = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
-    diagnostico = db.relationship('Diagnostico', backref=db.backref('evaluaciones', uselist=True))
+    diagnostico__rel = db.relationship('Diagnostico', backref='evaluacion_paciente')
+    especialista__rel = db.relationship('Especialista', backref='evaluacion_paciente')
+    test_resultado__rel = db.relationship('TestResultado', backref='evaluacion_paciente')
 
-    def __init__(self, id_diagnostico, especialista_id, resultado_id, fecha_evaluacion):
+    def __init__(self, id_diagnostico, especialista_id, resultado_id, fecha_evaluacion=None):
         self.id_diagnostico = id_diagnostico
         self.especialista_id = especialista_id
         self.resultado_id = resultado_id
-        self.fecha_evaluacion = fecha_evaluacion
+        if fecha_evaluacion is None:
+            self.fecha_evaluacion = datetime.now(timezone.utc)
+        else:
+            self.fecha_evaluacion = fecha_evaluacion
